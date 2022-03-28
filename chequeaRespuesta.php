b4c5707ebd6dfd9cfe3e6a90e2ca4907a7bdd019
esmela27 <esmeralda.lopezarellano98@gmail.com>
2022-03-28 20:20:40 +0200

    respuesta

---
+++ chequeaRespuesta.php
@@ -0,0 +1,23 @@
+<?php
+include('misfunciones.php'); //Incluye el archivo misfunciones
+$mysqli = conectaBBDD(); // la variable $mysqli coge la funcion conectaBBDD() de misfuncones
+
+$respuesta = $_POST['respuesta'];
+$numeroPregunta = $_POST['numeroPregunta'];
+//query mal hecha¡¡¡¡ SUSPENSO
+// $consulta = $mysqli->query(" SELECT * FROM `preguntas` WHERE `numero` = '$numeroPregunta' ");
+// $r = $consulta->fetch_array();
+
+$consulta = $mysqli -> prepare("SELECT correcta FROM `preguntas` WHERE `numero` = ? ");
+$consulta -> bind_param("s", $numeroPregunta);
+$consulta ->execute();
+$consulta ->store_result();
+$consulta ->bind_result($correcta);
+$consulta ->fetch();
+if ($correcta == $respuesta){
+    echo 'Acertaste';
+}
+else{
+    echo 'fallastes';
+}
+?>
--- partida.php
+++ partida.php
@@ -23,19 +23,19 @@ $r = $consulta->fetch_array();
                 <?php echo $r['enunciado']; ?>
             </button>
             <br><br>
-            <button class="btn btn-warning col-12 "  onclick="chequeaRespuesta ('1');">
+            <button class="btn btn-warning col-12 "  onclick="chequeaRespuesta ('1','<?php echo $r['numero']; ?>');">
                 <?php echo $r['r1']; ?>
             </button>
             <br>
-            <button class="btn btn-warning col-12" onclick="chequeaRespuesta ('2');">
+            <button class="btn btn-warning col-12" onclick="chequeaRespuesta ('2','<?php echo $r['numero']; ?>');">
                 <?php echo $r['r2']; ?>
             </button>
             <br>
-            <button class="btn btn-warning  col-12" onclick="chequeaRespuesta ('3');">
+            <button class="btn btn-warning  col-12" onclick="chequeaRespuesta ('3','<?php echo $r['numero']; ?>');">
                 <?php echo $r['r3']; ?>
             </button>
             <br>
-            <button class="btn btn-warning  col-12" onclick="chequeaRespuesta ('4');">
+            <button class="btn btn-warning  col-12" onclick="chequeaRespuesta ('4','<?php echo $r['numero']; ?>');">
                 <?php echo $r['r4']; ?>
             </button>
             <br>
@@ -48,7 +48,10 @@ $r = $consulta->fetch_array();
 </div>
 <div id="cargaRespuesta"></div>
 <script>
-    function chequeaRespuesta(_respuesta){
-        $('#cargaRespuesta').load();
+    function chequeaRespuesta(_respuesta, _numero){
+        $('#cargaRespuesta').load('chequeaRespuesta.php', {
+            respuesta: _respuesta,
+            numeroPregunta: _numero,
+        });
     }
 </script>
